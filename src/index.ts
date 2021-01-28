cac854c56c0e43738a716f051610fce6a1c776a0
chris <skylake112@outlook.com>
2021-01-28 15:54:49 -0700

    wire up event listeners

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import './styles/index.scss';
 
+const reset = document.querySelector('#reset')
+const merge = document.querySelector('#merge')
+const quick  = document.querySelector('#quick')
+const heap = document.querySelector('#heap')
+const bubble = document.querySelector('#bubble')
+const insertion = document.querySelector('#insertion') 
+
 const canvas = <HTMLCanvasElement>document.getElementById('canvas')
 const ctx = canvas.getContext('2d');
 
@@ -7,4 +14,4 @@ if(!ctx || !(ctx instanceof CanvasRenderingContext2D)) {
   throw new Error("Couldn't get canvas") 
 }
 
-ctx.fillRect(100, 100, 100, 100)
+ctx.fillRect(100, 25, 100, 100)
