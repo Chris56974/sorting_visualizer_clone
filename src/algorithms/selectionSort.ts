60edecee01eb5bee5484c407876dc0617137d780
chris <skylake112@outlook.com>
2021-01-30 14:48:47 -0700

    Add insertion sort boiler plate

--- src/algorithms/selectionSort.ts
+++ src/algorithms/selectionSort.ts
@@ -2,7 +2,7 @@ export const selectionSort = (array: number[]) => {
   for (let i = 0; i < array.length; i++) {
     let min = i;
     for (let j = i + 1; j < array.length; j++) {
-      if (array[j] < array[min]) min = j;  // find the smallest and set it to i
+      if (array[j] < array[min]) min = j; 
     }
     if (min != i) {
       let tmp = array[i];
--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,19 @@ import './styles/index.scss';
 import { canvasInit } from './canvas'
 import { randomizeArray } from './helpers/randomizeArray';
 import { bubbleSort } from './algorithms/bubbleSort';
+import { selectionSort } from './algorithms/selectionSort';
+import { insertionSort } from './algorithms/insertionSort';
 
 const LOWER_BOUND = 1
 const UPPER_BOUND = 50
 
 const reset = document.querySelector('#reset')
+const heap = document.querySelector('#heap')
 const merge = document.querySelector('#merge')
 const quick = document.querySelector('#quick')
-const heap = document.querySelector('#heap')
 const bubble = document.querySelector('#bubble')
-const insertion = document.querySelector('#insertion')
 const selection = document.querySelector('#selection')
+const insertion = document.querySelector('#insertion')
 
 const array: number[] = []; 
 randomizeArray(array, LOWER_BOUND, UPPER_BOUND) 
@@ -25,7 +27,12 @@ reset?.addEventListener('click', () => {
 bubble?.addEventListener('click', () => { 
   bubbleSort(array)
 })
+selection?.addEventListener('click', () => {
+  selectionSort(array)
+})
+insertion?.addEventListener('click', () => {
+  insertionSort(array)
+})
 // merge?.addEventListener('click', TODO)
 // quick?.addEventListener('click', TODO)
 // heap?.addEventListener('click', TODO)
-// insertion?.addEventListener('click', TODO)
